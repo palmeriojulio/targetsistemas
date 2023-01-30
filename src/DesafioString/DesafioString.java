f1ae65fd8d9c464402b7b7aa777236c60c47938d
Palmério <palmerio_julio@hotmail.com>
2023-01-30 10:26:10 -0300

    Atualizando

--- src/DesafioString/DesafioString.java
+++ src/DesafioString/DesafioString.java
@@ -6,6 +6,15 @@ public class DesafioString {
 
     public static void main(String[] args){
 
+        /**
+         * Escreva um programa que inverta os caracteres de um string.
+         * IMPORTANTE:
+         *
+         * a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser
+         * previamente definida no código;
+         * b) Evite usar funções prontas, como, por exemplo, reverse;
+         */
+
         Scanner entreda = new Scanner(System.in);
         System.out.print("Informe uma palavra para ser invertida: ");
         String palavra = entreda.nextLine();
